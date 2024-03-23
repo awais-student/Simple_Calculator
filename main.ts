44db101592cadd602c84151c9073b9af8cc548b7
AwaisArain <158747214+awais-student@users.noreply.github.com>
2024-03-24 02:07:34 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt([
+    {message:"Enter First Number", type:"number", name:"firstNumber"},
+    {message:"Enter second Number", type:"number", name:"secondNumber"},
+    {
+        message:"select one operator to perform operations?",
+        type:"list",
+        name:"operator",
+        choices:["Multiplication", "division", "Addition",  "Substraction"],
+    },
+]);
+
+if(answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if(answer.operator === "Substraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if(answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if(answer.operator === "division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else{
+    console.log("Invalid Input");
+}
+
